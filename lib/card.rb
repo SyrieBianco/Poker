affa183b2dca7af45a58bdc0ff0bbc7d54452765
App Academy Student <student@appacademy.io>
2017-06-07 11:27:41 -0700

    Initial commit

---
+++ lib/card.rb
@@ -0,0 +1,28 @@
+class Card
+
+  VALUES = {
+    'Ace' => 14,
+    'King' => 13,
+    'Queen' => 12,
+    'Jack' => 11,
+    '10' => 10,
+    '9' => 9,
+    '8' => 8,
+    '7' => 7,
+    '6' => 6,
+    '5' => 5,
+    '4' => 4,
+    '3' => 3,
+    '2' => 2,
+    '1' => 1
+  }
+
+  def initialize(type, suit)
+    @type = type
+    @value =
+    @suit = suit
+  end
+
+
+
+end
